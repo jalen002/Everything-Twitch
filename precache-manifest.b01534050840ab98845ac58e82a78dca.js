9374876ed61c284baa6ca5c6f283c505cf6049f8
Jalen Halverson <Jhalvey08@gmail.com>
2020-02-16 10:47:24 -0700

    Updates

---
+++ precache-manifest.b01534050840ab98845ac58e82a78dca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3716fe83971e5bf5ef13079fd40d7b9",
+    "url": "/Everything-Twitch/index.html"
+  },
+  {
+    "revision": "775ca83c4befb103254a",
+    "url": "/Everything-Twitch/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9ed002ad47c831e058a9",
+    "url": "/Everything-Twitch/static/js/2.8f745921.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Everything-Twitch/static/js/2.8f745921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775ca83c4befb103254a",
+    "url": "/Everything-Twitch/static/js/main.d037e301.chunk.js"
+  },
+  {
+    "revision": "d4a09fec3af4b0a9583b",
+    "url": "/Everything-Twitch/static/js/runtime-main.147f7f85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Everything-Twitch/static/media/logo.5d5d9eef.svg"
+  }
+]);
