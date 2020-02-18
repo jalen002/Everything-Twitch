6d1071c63b4cf4e36bb8e5591125d53f41881444
Jalen Halverson <Jhalvey08@gmail.com>
2020-02-17 19:03:28 -0700

    app bar

--- src/index.js
+++ src/index.js
@@ -4,26 +4,39 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import Commands from './view/commands/Commands';
 
-const history = createBrowserHistory();
-
 const routing = (
-  <Router basename={process.env.PUBLIC_URL} history={history}>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/commands">Commands</Link>
-        </li>
-      </ul>
-      <Route exact path="/" component={App} />
-      <Route path="/commands" component={Commands} />
-    </div>
+  <Router basename={process.env.PUBLIC_URL}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">
+          Mint Patty 17
+        </Typography>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/commands">Commands</Link>
+            </li>
+          </ul>
+        </div>
+      </Toolbar>
+    </AppBar>
+
+    <Route exact path="/" component={App} />
+    <Route path="/commands" component={Commands} />
   </Router>
 )
 
