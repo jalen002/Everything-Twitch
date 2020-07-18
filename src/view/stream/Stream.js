7e72c38bb16fbd36797237723b1968c881c6cb04
Jalen Halverson <Jhalvey08@gmail.com>
2020-07-17 22:51:19 -0700

    full height and width

--- src/view/stream/Stream.js
+++ src/view/stream/Stream.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import ReactPlayer from 'react-player';
 import { TwitchEmbed } from 'react-twitch-embed';
 
 
@@ -13,11 +12,13 @@ class Stream extends Component {
       // Add a placeholder for the Twitch embed
       <div>
         <TwitchEmbed
-          channel="mintpatty17"
-          id="mintpatty17"
-          theme="dark"
+          channel='mintpatty17'
+          id='mintpatty17'
+          theme='dark'
           muted
           onVideoPause={() => console.log(':(')}
+          height='100vh'
+          width='100%'
         />
       </div>
     )
